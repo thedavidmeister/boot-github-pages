aec9084666a1d34ec56f31cdc65bc26a1a0761ad
David Meister <thedavidmeister@gmail.com>
2017-09-16 22:47:48 +1000

    ensure master repo for github-pages task

--- src/thedavidmeister/boot_github_pages.clj
+++ src/thedavidmeister/boot_github_pages.clj
@@ -1,11 +1,17 @@
 (ns thedavidmeister.boot-github-pages
  (:require
   boot.core
-  boot.git))
+  boot.git
+  boot.util))
 
 (boot.core/deftask github-pages
  "Deploy to github pages"
  []
  (boot.core/with-pass-thru
   [_]
-  (prn (boot.git/clean?))))
+  (when (not (= "master" (boot.git/branch-current)))
+   (boot.util/exit-error
+    (boot.util/fail "Attempted to deploy to Github Pages from the wrong branch. Checkout master and try again.\n")))
+  (when (boot.git/dirty?)
+   (boot.util/exit-error
+    (boot.util/fail "Attempted to deploy to Github Pages with a dirty repo. Commit your changes and try again.\n")))))
