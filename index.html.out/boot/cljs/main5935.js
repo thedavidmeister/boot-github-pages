25c31a9a637198fe2ea2cabcb2bacd95f4878ff8
David Meister <thedavidmeister@gmail.com>
2017-09-17 17:38:02 +1000

    adding identity for git-commit-push-all!

--- index.html.out/boot/cljs/main5935.js
+++ index.html.out/boot/cljs/main5935.js
@@ -1,5 +1,5 @@
 // Compiled by ClojureScript 1.9.908 {:static-fns true, :optimize-constants true}
-goog.provide('boot.cljs.main5928');
+goog.provide('boot.cljs.main5935');
 goog.require('cljs.core');
 goog.require('cljs.core.constants');
 goog.require('pages.index');
